ce714a1ef1bb46325437680ed8992e6518e0d995
Soner Sayakci <s.sayakci@shopware.com>
2020-03-01 18:54:27 +0100

    Add vpn config

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -6,5 +6,6 @@
 
   networking.extraHosts = ''
     127.0.0.1 mysql
+    10.10.0.1 cloud.internal
   '';
 }
---
+++ modules/desktop/vpn.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, ... }:
+
+{
+    networking.wireguard.interfaces = {
+        wg0 = {
+            ips = [ "10.10.0.2/24" ];
+            privateKeyFile = "/home/shyim/.config/wireguard/private_key";
+
+            peers = [
+                {
+                    publicKey = "cujM0Bf/zKM3K/KzrThO6WyBs/AfAJNcJymy7/nNljA=";
+                    endpoint = "5.9.79.62:51820";
+                    allowedIPs = [ "10.10.0.1" ];
+                }
+            ];
+        };
+    };
+}
