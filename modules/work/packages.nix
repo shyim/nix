2f56c671702e64cc2492f2ee844b49e3015b22e0
Soner Sayakci <s.sayakci@shopware.com>
2020-07-01 12:07:37 +0200

    Update packges

--- modules/work/packages.nix
+++ modules/work/packages.nix
@@ -8,5 +8,7 @@
     ansible
     go-jira
     zoom-us
+    chromium
+    kubectl
   ];
 }
--- modules/work/services.nix
+++ modules/work/services.nix
@@ -2,4 +2,7 @@
 
 {
   services = { printing = { enable = true; }; };
+  services.davmail.enable = true;
+  services.davmail.url = "https://owa.shopware.com/EWS/Exchange.asmx";
+  services.davmail.config.davmail.mode = "EWS";
 }
